bfd580eae80744b91662d30826346823c9ed5364
Adam Gibson <adam@skymind.io>
2015-06-25 15:25:15 -0700

    fix batch size

--- src/main/java/org/deeplearning4j/SparkGpuExample.java
+++ src/main/java/org/deeplearning4j/SparkGpuExample.java
@@ -4,6 +4,13 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
+import akka.actor.ActorSystem;
+import akka.actor.ExtendedActorSystem;
+import akka.serialization.JavaSerializer;
+import akka.serialization.Serialization;
+import akka.serialization.SerializationExtension;
+import akka.serialization.Serializer;
 import org.apache.commons.io.FileUtils;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
@@ -22,6 +29,7 @@ import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.optimize.api.IterationListener;
 import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
 import org.deeplearning4j.spark.impl.multilayer.SparkDl4jMultiLayer;
+import org.deeplearning4j.spark.util.SerializationTester;
 import org.deeplearning4j.util.SerializationUtils;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
@@ -52,20 +60,26 @@ public class SparkGpuExample {
         //This is where you configure your deep-belief net
         MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
                 .momentum(0.9).iterations(10)
-                .weightInit(WeightInit.XAVIER).batchSize(10000)
+                .weightInit(WeightInit.XAVIER).batchSize(100)
                 .lossFunction(LossFunctions.LossFunction.RMSE_XENT)
                 .nIn(784).nOut(10).layer(new RBM())
                 .list(4).hiddenLayerSizes(600, 500, 400)
                 .override(3, new ClassifierOverride()).build();
 
+      /*  MultiLayerNetwork network = new MultiLayerNetwork(conf);
+        network.init();
+        SerializationTester.testSerialization(sc.env().actorSystem(),network);
+*/
         //and here you bring Spark and the MultiLayer neural net together...
         System.out.println("Initializing network");
         SparkDl4jMultiLayer master = new SparkDl4jMultiLayer(sc,conf);
+
         System.out.println("Loading data...");
         DataSet d = !(new File("dataset.ser").exists()) ? new MnistDataSetIterator(60000,60000).next() : (DataSet) SerializationUtils.readObject(new File("dataset.ser"));
         if(!(new File("dataset.ser").exists()))
             SerializationUtils.saveObject(d,new File("dataset.ser"));
         d.shuffle();
+
         System.out.println("Shuffled data set");
         SplitTestAndTrain split = d.splitTestAndTrain(0.8);
         System.out.println("Split data");
